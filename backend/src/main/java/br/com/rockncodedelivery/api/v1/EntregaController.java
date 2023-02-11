ca6ab07e5f23f76550a0481cbdff0878e998c4f7
Aleff R. Dev <aleff.remberto@hotmail.com>
2023-02-11 10:08:22 -0300

    Update EntregaController, modifica pathvariable para requestParam
    Update EntregaController, modifica pathvariable para requestParam

--- backend/src/main/java/br/com/rockncodedelivery/api/v1/EntregaController.java
+++ backend/src/main/java/br/com/rockncodedelivery/api/v1/EntregaController.java
@@ -2,7 +2,9 @@ package br.com.rockncodedelivery.api.v1;
 
 import br.com.rockncodedelivery.api.v1.dto.EntregaRequest;
 import br.com.rockncodedelivery.domain.entities.Entrega;
+import br.com.rockncodedelivery.domain.entities.Entregador;
 import br.com.rockncodedelivery.domain.service.impl.EntregaServiceImpl;
+import br.com.rockncodedelivery.external.dto.directions.ResponseDirectionsApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -40,8 +42,23 @@ public class EntregaController {
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletarPorId(@PathVariable Long id){
+    public void deletarPorId(@PathVariable Long id) {
         entregaService.deletarPorId(id);
+    }
+
+    @GetMapping("rotas/")
+    public ResponseDirectionsApi obterMelhorRota(@RequestParam("enderecoOrigem") String enderecoOrigem, @RequestParam("enderecoFinal") String enderecoFinal) {
+        return entregaService.obterMelhorRota(enderecoOrigem, enderecoFinal);
+    }
+
+    @PutMapping("vincular/")
+    public void vincularEntregadorAEntrega(@RequestParam("idEntregador")Long idEntregador, @RequestParam("idEntrega") Long idEntrega) {
+        entregaService.vincularEntregadorAEntrega(idEntregador, idEntrega);
+    }
+
+    @PutMapping("{id}")
+    public void proximoStatus(@PathVariable Long idEntrega) {
+        entregaService.proximoStatus(idEntrega);
     }
 
 }
