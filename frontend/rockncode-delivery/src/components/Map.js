5fd822e60c229e79beb0157a47a13e9ff60847e3
Sabrina Drashen <sdrashen@gmail.com>
2023-02-11 09:38:14 -0300

    cria navegação entre telas

--- frontend/rockncode-delivery/src/components/Map.js
+++ frontend/rockncode-delivery/src/components/Map.js
@@ -1,5 +1,5 @@
-import React, { useCallback, useState } from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React, { useCallback, useEffect, useState } from 'react'
+import { DirectionsRenderer, GoogleMap, Marker, TrafficLayer, useJsApiLoader } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '612px',
@@ -30,11 +30,13 @@ function Map() {
 
   const [map, setMap] = useState(null)
   const [directions, setDirections] = useState(null)
-  const onLoad = useCallback(function callback(map) {
-  
+  const [position1, setPosition1] = useState({ lat: 37.7749, lng: -122.4194 });
+  const [position2, setPosition2] = useState({ lat: 40.7128, lng: -74.0060 });
+  const [route, setRoute] = useState(null);
+  const onLoad = async () => {
     const directionsService = new window.google.maps.DirectionsService();
-    
-  
+    const directionsDisplay = new window.google.maps.DirectionsRenderer();
+
     directionsService.route(
       {
         origin: origin,
@@ -43,18 +45,22 @@ function Map() {
       },
       (result, status) => {
         if (status === window.google.maps.DirectionsStatus.OK) {
-          setDirections(result);
+          setRoute(result);
         } else {
           console.error(`error fetching directions ${result}`);
         }
       }
     );
-    setMap(map)
-  }, [])
+  };
 console.log("directions", directions)
   const onUnmount = useCallback(function callback(map) {
     setMap(null)
   }, [])
+
+  useEffect(() => {
+
+    onLoad();
+  }, []);
 
   return isLoaded ? (
       <GoogleMap
@@ -64,7 +70,14 @@ console.log("directions", directions)
         onLoad={onLoad}
         onUnmount={onUnmount}
       >
-        <></>
+        <TrafficLayer
+                  onLoad={onLoad}
+                />
+                <Marker position={position1} onClick={() => setPosition1({ lat: 40.7128, lng: -74.0060 })} />
+                <Marker position={position2} onClick={() => setPosition2({ lat: 37.7749, lng: -122.4194 })} />
+                {route && <DirectionsRenderer options={{
+                  directions: route
+                }} />}
       </GoogleMap>
   ) : <></>
 }
--- frontend/rockncode-delivery/src/index.js
+++ frontend/rockncode-delivery/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Dashboard from './screens/Dashboard';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard/>
+  },
+  {
+    path: "/Login",
+    element: <div>About</div>,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
