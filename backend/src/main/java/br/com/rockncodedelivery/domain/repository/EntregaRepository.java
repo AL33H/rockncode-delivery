78b1c5b1ee73663cb9009cdd608958f28b8ec095
Jardel Barbosa <86034190+Jardel894@users.noreply.github.com>
2023-02-11 16:14:21 -0300

    Atualizacao do repository

--- backend/src/main/java/br/com/rockncodedelivery/domain/repository/EntregaRepository.java
+++ backend/src/main/java/br/com/rockncodedelivery/domain/repository/EntregaRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface EntregaRepository extends JpaRepository<Entrega, Long> {
+public interface              EntregaRepository extends JpaRepository<Entrega, Long> {
 
     List<Entrega> findByStatus(StatusEntrega status);
 }
