7f4e7d512c8a911fcdbca05bc4a902321a90f7c0
Jardel Barbosa <86034190+Jardel894@users.noreply.github.com>
2023-02-10 22:19:00 -0300

    Estrutura do backend

---
+++ src/main/java/br/com/rockncodedelivery/RockncodedeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.rockncodedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockncodedeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockncodedeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rockncodedelivery/RockncodedeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rockncodedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockncodedeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
