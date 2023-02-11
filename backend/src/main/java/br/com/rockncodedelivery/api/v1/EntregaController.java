4c199386103c00f137bebc400a30e540c47272e7
Aleff R. Dev <aleff.remberto@hotmail.com>
2023-02-11 07:04:56 -0300

    Adicionando endpoints no controller de entrega

--- backend/src/main/java/br/com/rockncodedelivery/api/v1/EntregaController.java
+++ backend/src/main/java/br/com/rockncodedelivery/api/v1/EntregaController.java
@@ -4,18 +4,44 @@ import br.com.rockncodedelivery.api.v1.dto.EntregaRequest;
 import br.com.rockncodedelivery.domain.entities.Entrega;
 import br.com.rockncodedelivery.domain.service.impl.EntregaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/entrega")
 public class EntregaController {
 
-    @Autowired
     EntregaServiceImpl entregaService;
 
+    @Autowired
+    public EntregaController(EntregaServiceImpl entregaService) {
+        this.entregaService = entregaService;
+    }
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Entrega gerarNovaEntrega(@RequestBody EntregaRequest entregaRequest) {
         return entregaService.gerarNovaEntrega(entregaRequest);
+    }
+
+    @GetMapping("/${id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Entrega buscarPorId(@RequestParam(value = "id") Long id) {
+        return entregaService.buscarPorId(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public List<Entrega> buscarTodos() {
+        return entregaService.buscarTodos();
+    }
+
+    @DeleteMapping("/${id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarPorId(@RequestParam(value = "id") Long id){
+        entregaService.deletarPorId(id);
     }
 
 }
--- backend/src/main/java/br/com/rockncodedelivery/domain/service/impl/EntregaServiceImpl.java
+++ backend/src/main/java/br/com/rockncodedelivery/domain/service/impl/EntregaServiceImpl.java
@@ -12,25 +12,30 @@ import br.com.rockncodedelivery.external.dto.distanceMatrix.ResponseDistanceMatr
 import br.com.rockncodedelivery.external.dto.geocode.ResponseGeocodeApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EntregaServiceImpl implements EntregaService {
 
-    @Autowired
-    private GoogleAPI externalApi;
+    private ExternalApi externalApi;
 
-    @Autowired
     private EntregaRepository entregaRepository;
-
-    public EntregaServiceImpl(GoogleAPI externalApi) {
+    @Autowired
+    public EntregaServiceImpl(GoogleAPI externalApi, EntregaRepository entregaRepository) {
         this.externalApi = externalApi;
+        this.entregaRepository = entregaRepository;
     }
 
     public Entrega gerarNovaEntrega(EntregaRequest entregaRequest) {
 
         ResponseGeocodeApi geocodeOrigem = externalApi.buscaGeolocalizacaoEndereco(entregaRequest.getEnderecoOrigem());
         ResponseGeocodeApi geocodeDestino = externalApi.buscaGeolocalizacaoEndereco(entregaRequest.getEnderecoDestino());
-
 
         LocalizacaoOrigem localizacaoOrigem = new LocalizacaoOrigem(
                 geocodeOrigem.getResults().get(0).getGeometry().getLocation().getLat(),
@@ -46,9 +51,8 @@ public class EntregaServiceImpl implements EntregaService {
 
         String text = responseDistanceMatrix.getRows().get(0).getElements().get(0).getDistance().getText()
                 .replace("km", "")
-                .replace(" ","")
-                .replace("m","");
-
+                .replace("m", "")
+                .replace(" ", "");
 
         Entrega entrega = Entrega.builder()
                 .nomeSolicitante(entregaRequest.getNomeSolicitante())
@@ -60,14 +64,18 @@ public class EntregaServiceImpl implements EntregaService {
         System.out.println(entrega);
 
         return entregaRepository.save(entrega);
-        
     }
 
+    public Entrega buscarPorId(@RequestParam(value = "id") Long id) {
+        Optional<Entrega> entrega = entregaRepository.findById(id);
+        return entrega.orElseThrow(IllegalStateException::new);
+    }
 
-    //todo
-    //new
-    //getAll(Receber enum)
-    //delete
+    public List<Entrega> buscarTodos(){
+        return entregaRepository.findAll();
+    }
 
-
+    public void deletarPorId(@RequestParam(value = "id") Long id){
+        entregaRepository.deleteById(id);
+    }
 }
